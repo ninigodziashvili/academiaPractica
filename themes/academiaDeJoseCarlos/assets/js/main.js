c5cbe8887db5c0eebdfb0264f61a5df79f7bf0cd
Nino Godziashvili <nino.godziashvili@gmail.com>
2025-07-02 17:34:12 +0200

    changes for the merging

--- themes/academiaDeJoseCarlos/assets/js/main.js
+++ themes/academiaDeJoseCarlos/assets/js/main.js
@@ -174,23 +174,3 @@ window.addEventListener('scroll', function () {
     navbar.classList.remove('scrolled');
   }
 });
-
-// Navegador
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-link");
-  const navLinks = document.querySelectorAll(".nav-link li");
-
-  burger.
-  ("click", () => {
-    nav.classList.toggle("nav-link-activated");
-    burger.classList.toggle("toggle")
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
-      }
-    })
-  });
-}
